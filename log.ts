57a6582d079153ae8180956f80e40034f061fdf4
Tomofumi Chiba <tomofumi.chiba@gmail.com>
2021-07-03 20:36:57 +0900

    fix

--- log.ts
+++ log.ts
@@ -1,6 +1,13 @@
 let count = 0;
 console.log("debug 01");
 console.log(Deno.env.toObject());
+console.log(Deno.permissions.query({name: "read"}));
+console.log(Deno.permissions.query({name: "write"}));
+console.log(Deno.permissions.query({name: "env"}));
+console.log(Deno.permissions.query({name: "net"}));
+console.log(Deno.permissions.query({name: "run"}));
+console.log(Deno.permissions.query({name: "plugin"}));
+console.log(Deno.permissions.query({name: "hrtime"}));
 
 // deno-lint-ignore no-explicit-any
 addEventListener("fetch", (event: any) => {
