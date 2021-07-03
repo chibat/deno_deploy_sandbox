204e09c200e3d6153f52b1eb4d1fbe731dc803d2
Tomofumi Chiba <tomofumi.chiba@gmail.com>
2021-07-03 19:26:13 +0900

    fix

--- log.ts
+++ log.ts
@@ -1,7 +1,8 @@
 let count = 0;
+console.log("debug 01");
 // deno-lint-ignore no-explicit-any
 addEventListener("fetch", (event: any) => {
-
+  console.log("debug 02");
   console.log(`Received request #${++count} to ${event.request.url}`);
   event.respondWith(
     new Response("Hello world", {
