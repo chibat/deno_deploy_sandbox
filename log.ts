14b88d4815265d7257040305680417e77f34ade2
Tomofumi Chiba <tomofumi.chiba@gmail.com>
2021-07-03 19:24:03 +0900

    fix

--- log.ts
+++ log.ts
@@ -1,6 +1,7 @@
 let count = 0;
 // deno-lint-ignore no-explicit-any
 addEventListener("fetch", (event: any) => {
+
   console.log(`Received request #${++count} to ${event.request.url}`);
   event.respondWith(
     new Response("Hello world", {
